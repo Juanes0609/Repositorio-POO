e04c7c7e922141a3d041a7cae9a19d0a95a16717
Juanes0609 <juane.chicao@uqvirtual.edu.co>
2025-05-25 09:40:05 -0500

    Delete proyecto_final directory

--- proyecto_final/src/main/java/co/edu/uniquindio/poo/Model/SaldoInsuficienteException.java
+++
@@ -1,6 +0,0 @@
-package co.edu.uniquindio.poo.Model;
-public class SaldoInsuficienteException extends Exception {
-    public SaldoInsuficienteException(String mensaje) {
-        super(mensaje); 
-    }
-}
